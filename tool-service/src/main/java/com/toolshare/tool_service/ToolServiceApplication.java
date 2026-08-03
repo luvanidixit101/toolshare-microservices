b2ccb89a3659160c4537db70b50633ae62c38b76
luvanidixit101 <luvanidixit101@gmail.com>
2026-08-03 19:13:33 +0530

    add dependency for all services

---
+++ tool-service/src/main/java/com/toolshare/tool_service/ToolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.toolshare.tool_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolServiceApplication.class, args);
+	}
+
+}
---
+++ tool-service/src/test/java/com/toolshare/tool_service/ToolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolshare.tool_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
